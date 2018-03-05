1056c50627b98cf2f20b9ae32dab931c4687a7f6
vamsiporuri <vamsi.poruri@gmail.com>
2018-03-04 23:51:30 -0500

    springboot helloworld

---
+++ helloworld/src/main/java/com/test/HomeController.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/hello")  
+    public String hello(){  
+	    return"Hello SpringBoot!";  
+    }  
+}
